51bc8fc9c94b82b7706b351ff8279778811d5992
ponapalt <ponapalt@shillest.net>
2020-09-12 06:46:42 +0900

    puma single mode if WEB_CONCURRENCY = 1

--- config/puma.rb
+++ config/puma.rb
@@ -12,6 +12,20 @@ end
 environment ENV.fetch('RAILS_ENV') { 'development' }
 workers     ENV.fetch('WEB_CONCURRENCY') { 2 }
 
+worker_num = ENV.fetch('WEB_CONCURRENCY') { 2 }.to_i
+
+if worker_num > 1 then
+  workers worker_num
+  preload_app!
+  on_worker_boot do
+    ActiveSupport.on_load(:active_record) do
+      ActiveRecord::Base.establish_connection
+    end
+  end
+end
+
+=begin
+
 preload_app!
 
 on_worker_boot do
@@ -19,5 +33,8 @@ on_worker_boot do
     ActiveRecord::Base.establish_connection
   end
 end
+
+=end
+
 
 plugin :tmp_restart
