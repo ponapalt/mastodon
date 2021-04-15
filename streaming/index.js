a184deb4c74d9463b84a89f662ef7dcb7df7de4c
ponapalt <ponapalt@shillest.net>
2021-04-15 10:34:15 +0900

    Revert "Revert "debug test""
    This reverts commit 6247955dc328f831e65311df5f008feed50f89d1.

--- streaming/index.js
+++ streaming/index.js
@@ -994,6 +994,8 @@ const startWorker = (workerId) => {
   };
 
   wss.on('connection', (ws, req) => {
+    try {
+
     const location = url.parse(req.url, true);
 
     req.requestId     = uuid.v4();
@@ -1052,6 +1054,12 @@ const startWorker = (workerId) => {
     if (location.query.stream) {
       subscribeWebsocketToChannel(session, firstParam(location.query.stream), location.query);
     }
+    
+    } catch (err) {
+      log.error(err);
+      console.trace();
+    }
+
   });
 
   setInterval(() => {
