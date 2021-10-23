add0c59ea41299ac58911bf29343da87c2180036
ponapalt <ponapalt@shillest.net>
2021-10-23 09:14:07 +0900

    full-text search custom

--- app/models/concerns/account_interactions.rb
+++ app/models/concerns/account_interactions.rb
@@ -302,8 +302,11 @@ module AccountInteractions
   def relations_map(account_ids, domains = nil, **options)
     relations = {
       blocked_by: Account.blocked_by_map(account_ids, id),
-      following: Account.following_map(account_ids, id),
     }
+    
+    relations.merge!({
+      following: Account.following_map(account_ids, id),
+    }) unless options[:skip_following]
 
     return relations if options[:skip_blocking_and_muting]
 
