87a47f6d2b1ed098e8777463dcbb1913b241f855
ponapalt <ponapalt@shillest.net>
2021-10-23 09:17:41 +0900

    longer request wait

--- app/lib/request.rb
+++ app/lib/request.rb
@@ -20,7 +20,8 @@ class Request
   # We enforce a 5s timeout on DNS resolving, 5s timeout on socket opening
   # and 5s timeout on the TLS handshake, meaning the worst case should take
   # about 15s in total
-  TIMEOUT = { connect: 5, read: 10, write: 10 }.freeze
+  # TIMEOUT = { connect: 5, read: 10, write: 10 }.freeze
+  TIMEOUT = { connect: 15, read: 30, write: 30 }.freeze
 
   include RoutingHelper
 
