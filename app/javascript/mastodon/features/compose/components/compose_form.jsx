b9be787f198b4657e109e9f5c6216605956ae17f
C.Ponapalt <ponapalt@shillest.net>
2025-09-18 04:24:13 +0900

    quoteをcompose直下に

--- app/javascript/mastodon/features/compose/components/compose_form.jsx
+++ app/javascript/mastodon/features/compose/components/compose_form.jsx
@@ -31,6 +31,8 @@ import { UploadForm } from './upload_form';
 import { Warning } from './warning';
 import { ComposeQuotedStatus } from './quoted_post';
 import { VisibilityButton } from './visibility_button';
+import { VisibilityDropdown } from './visibility_dropdown';
+import { QuotePolicyDropdown } from './quote_policy_dropdown';
 
 const allowedAroundShortCode = '><\u0085\u0020\u00a0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000\u2028\u2029\u0009\u000a\u000b\u000c\u000d';
 
@@ -263,7 +265,8 @@ class ComposeForm extends ImmutablePureComponent {
           <EditIndicator />
 
           <div className='compose-form__dropdowns'>
-            <VisibilityButton disabled={this.props.isEditing} />
+            <VisibilityDropdown disabled={this.props.isEditing} />
+            <QuotePolicyDropdown disabled={this.props.isEditing} />
             <LanguageDropdown />
           </div>
 
